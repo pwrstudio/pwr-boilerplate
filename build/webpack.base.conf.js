4ae46b5f6c2fd377d11e30298d31e7d3b3900486
pwrstudio <pwrpntr@gmail.com>
2017-03-09 23:24:25 +0100

    more prefixin

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -92,7 +92,7 @@ module.exports = {
     loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
     postcss: [
       require('autoprefixer')({
-        browsers: ['last 2 versions']
+        browsers: ['last 2 versions', 'last 5 iOS versions']
       })
     ]
   }
